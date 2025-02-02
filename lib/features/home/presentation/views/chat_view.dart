1fa92d30ca12a0ba0c2baefb60093277b8fa21b4
Ahmedjk4 <botabasha1@gmail.com>
2025-02-02 02:44:38 +0200

    change chat to use cached network image

--- lib/features/home/presentation/views/chat_view.dart
+++ lib/features/home/presentation/views/chat_view.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -86,8 +87,9 @@ class _ChatViewState extends State<ChatView> {
                                             height: double.infinity,
                                             decoration: BoxDecoration(
                                               image: DecorationImage(
-                                                image: NetworkImage(
-                                                    message['link']),
+                                                image:
+                                                    CachedNetworkImageProvider(
+                                                        message['link']),
                                                 fit: BoxFit.contain,
                                               ),
                                             ),
