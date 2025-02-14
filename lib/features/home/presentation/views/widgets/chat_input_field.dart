e423e8149622ecaf0ed0ccc4d2a57315d6a46bb1
elmesery1014 <melmesery23@gmail.com>
2025-02-14 02:32:42 +0200

    test

--- lib/features/home/presentation/views/widgets/chat_input_field.dart
+++ lib/features/home/presentation/views/widgets/chat_input_field.dart
@@ -9,6 +9,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:permission_handler/permission_handler.dart';
 
+// update
 class ChatInputField extends StatefulWidget {
   final String userEmail;
   final ScrollController scrollController;
